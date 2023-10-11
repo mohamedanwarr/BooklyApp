90d1c6d222e6e83b5c6592722f906855212dc8c0
mohamedanwarr <mohamedanwarr111@gmail.com>
2023-10-11 19:57:57 +0300

    Splashviews and animations

---
+++ lib/Features/Splash/Presentation/Viewes/Widegts/slidingtext.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+class SlidingText extends StatelessWidget {
+  const SlidingText({
+    super.key,
+    required this.slidinganimation,
+  });
+
+  final Animation<Offset> slidinganimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideTransition(
+      position:slidinganimation,
+      child: const Text('Read Free Books'
+        , textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
--- lib/Features/Splash/Presentation/Viewes/Widegts/splash_view_body..dart
+++ lib/Features/Splash/Presentation/Viewes/Widegts/splash_view_body..dart
@@ -1,24 +1,57 @@
 import 'package:bookyapp/Core/utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:svg_flutter/svg.dart';
-class SplashViewbody extends StatelessWidget {
+import 'slidingtext.dart';
+
+class SplashViewbody extends StatefulWidget {
   const SplashViewbody({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  State<SplashViewbody> createState() => _SplashViewbodyState();
+}
 
+class _SplashViewbodyState extends State<SplashViewbody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidinganimation;
+
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidinganimation =
+        Tween<Offset>(begin: const Offset(0, 5), end: const Offset(0, 0))
+            .animate(animationController);
+    animationController.forward();
+    slidinganimation.addListener(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        SvgPicture.asset(AssetsData.logo,
-        width: 300,),
-        const SizedBox(height: 20),
-        const Text('Read Free Books'
-          ,textAlign: TextAlign.center,
-          )
+        SvgPicture.asset(
+          AssetsData.logo,
+          width: 300,
+        ),
+        const SizedBox(height: 10),
+        AnimatedBuilder(
+          animation: slidinganimation,
+          builder: (context, _) {
+            return SlidingText(slidinganimation: slidinganimation);
+          },
+        )
       ],
-
     );
   }
 }
