bb8308cd9e9d5bdcff5799b49a664731d2af13e1
mohamedanwarr <mohamedanwarr111@gmail.com>
2023-10-22 00:12:58 +0300

    impl fetch featured books remote source

---
+++ lib/Features/home/data/DataSources/Home_Remote_Data_Sources/Remote_Data_Sources.dart
@@ -0,0 +1,37 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:bookyapp/Core/utils/ApiServices.dart';
+import 'package:bookyapp/Features/home/Domain/Entities/entities.dart';
+import 'package:bookyapp/Features/home/data/Models/Books%20Model.dart';
+
+abstract class HomeRemoteDataSourcces {
+  Future<List<BookEntity>> FetchFeaturedBooks();
+
+  Future<List<BookEntity>> FetchNewBooks();
+}
+
+class HomeRemoteDataSourcceImpl extends HomeRemoteDataSourcces {
+  late final ApiServices apiServices;
+
+  @override
+  Future<List<BookEntity>> FetchFeaturedBooks() async {
+    var data = await apiServices.get(
+        endpoint: 'volumes?q=Harry Potter &Filtering =free-ebooks');
+    List<BookEntity> books = getBooksList(data);
+  return books;
+  }
+
+  List<BookEntity> getBooksList(Map<String, dynamic> data) {
+     List<BookEntity> books = [];
+    for (var bookMap in data['items']) {
+      books.add(BooksModels.fromJson(bookMap));
+    }
+    return books;
+  }
+
+  @override
+  Future<List<BookEntity>> FetchNewBooks() {
+    // TODO: implement FetchNewBooks
+    throw UnimplementedError();
+  }
+}
